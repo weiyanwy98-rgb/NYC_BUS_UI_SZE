2694d6f7281f5592c4a791022aa70e40a76a7ef9
weiyanwy98-rgb <weiyanwy98@gmail.com>
2025-12-02 11:44:55 +0800

    init commit

---
+++ backend/app.js
@@ -0,0 +1,119 @@
+import express from "express";
+import cors from "cors";
+import fs from "fs";
+import path from "path";
+import axios from "axios";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const NYC_API = "https://nyc-bus-engine-k3q4yvzczq-an.a.run.app/api/bus_trip";
+const DATA = path.join(process.cwd(), "data");
+
+// Simulate server cold-start delay
+let isReady = false;
+setTimeout(() => { isReady = true }, 3000);
+
+// ----- API: SERVER READY -----
+app.get("/api/bus_trip/ready", async (req, res) => {
+    try {
+        const response = await axios.get(`${NYC_API}/ready`);
+        return res.json(response.data);
+    } catch (err) {
+        return res.status(500).json({ ready: isReady });
+    }
+});
+
+// ----- API: VEHICLE REFS -----
+app.get("/api/bus_trip/getVehRef", async (req, res) => {
+    try {
+        console.log("trying NYC API...");
+        // 1. Call NYC API
+        const response = await axios.get(`${NYC_API}/getVehRef`);
+        const vehRefs = response.data;
+        // 2. Save to local file
+        fs.writeFileSync(`${DATA}/vehRef.json`, JSON.stringify(vehRefs, null, 2));
+        // 3. Return to client
+        res.json(vehRefs);
+
+    } catch (error) {
+        console.log("NYC API down, loading from local file...");
+
+        //4. Fallback to local JSON
+        const cache = JSON.parse(fs.readFileSync(`${DATA}/vehRef.json`, "utf-8"));
+        res.json(cache);
+    }
+});
+
+// ----- API: TRIP BY VEHREF -----
+app.get("/api/bus_trip/getBusTripByVehRef/:vehRef", async (req, res) => {
+    const { vehRef } = req.params;
+    try {
+        console.log("trying NYC API...");
+        // 1. Call NYC API
+        const response = await axios.get(`${NYC_API}/getBusTripByVehRef/${vehRef}`);
+        const tripData = response.data;
+        // 2. Save to local file
+        fs.writeFileSync(`${DATA}/tripsByVehRef/${vehRef}.geojson`, JSON.stringify(tripData, null, 2));
+        // 3. Return to client
+        return res.json(tripData);
+    } catch (error) {
+        console.log("NYC API down, loading from local file...");
+        //4. Fallback to local JSON
+        const file = `${DATA}/tripsByVehRef/${vehRef}.geojson`;
+        if (!fs.existsSync(file)) {
+            return res.status(404).json({ error: "Vehicle reference not found" });
+        }
+        const cache = JSON.parse(fs.readFileSync(file, "utf-8"));
+        res.json(cache);
+    }
+});
+
+// ----- API: PUBLISHED LINE NAMES -----
+app.get("/api/bus_trip/getPubLineName", async (req, res) => {
+    try {
+        console.log("trying NYC API...");
+        // 1. Call NYC API
+        const response = await axios.get(`${NYC_API}/getPubLineName`);
+        const lineNames = response.data;
+        // 2. Save to local file
+        fs.writeFileSync(`${DATA}/pubLineName.json`, JSON.stringify(lineNames, null, 2));
+        // 3. Return to client
+        return res.json(lineNames);
+    } catch (error) {
+        console.log("NYC API down, loading from local file...");
+        //4. Fallback to local JSON
+        if (!fs.existsSync(`${DATA}/pubLineName.json`)) {
+            return res.status(500).json({ error: "No cached data available" });
+        }
+        const cache = JSON.parse(fs.readFileSync(`${DATA}/pubLineName.json`, "utf-8"));
+        res.json(cache);
+    }
+});
+
+// ----- API: TRIP BY LINE NAME -----
+app.get("/api/bus_trip/getBusTripByPubLineName/:line", async(req, res) => {
+    const { line } = req.params;
+    const file = `${DATA}/tripsByLine/${line}.geojson`;
+    try {
+        console.log("trying NYC API...");
+        // 1. Call NYC API
+        const response = await axios.get(`${NYC_API}/getBusTripByPubLineName/${line}`);
+        const tripData = response.data;
+        // 2. Save to local file
+        fs.writeFileSync(file, JSON.stringify(tripData, null, 2));
+        // 3. Return to client
+        return res.json(tripData);
+    } catch (error) {
+        console.log("NYC API down, loading from local file...");
+        //4. Fallback to local JSON
+        if (!fs.existsSync(file)) {
+            return res.status(404).json({ error: "Line name not found" });
+        }
+        const cache = JSON.parse(fs.readFileSync(file, "utf-8"));
+        res.json(cache);
+    }
+});
+
+app.listen(5000, () => console.log("Server running on port 5000"));
